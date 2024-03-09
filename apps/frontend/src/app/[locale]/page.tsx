4e6983541f6876fe47e874daa76a5aec232976f4
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-03-09 15:27:00 +0100

    feat: switch to static rendering for languages

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -1,7 +1,26 @@
+import { unstable_setRequestLocale as setRequestLocale } from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
+
 import { useTranslations } from 'next-intl';
 
-export default function Index() {
+type Props = {
+  params: {
+    locale: string;
+  };
+};
+
+export async function generateMetadata({ params: { locale } }: Props) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+  return {
+    title: t('index'),
+  };
+}
+
+export default function Index({ params: { locale } }: Props) {
+  setRequestLocale(locale);
   const t = useTranslations('Index');
+
   /*
    * Replace the elements below with your own.
    *
