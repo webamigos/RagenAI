7012b49c17cad8aecdca0270a44d64dc0bc2d031
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-03-15 12:41:16 +0100

    feat: ege hardcoded

--- apps/frontend/src/app/api/sse/route.ts
+++ apps/frontend/src/app/api/sse/route.ts
@@ -3,7 +3,8 @@ import { Redis } from 'ioredis';
 
 const isDev = process.env.NODE_ENV === 'development';
 // TODO: might broke build on local machine?
-export const runtime = isDev ? 'nodejs' : 'edge';
+// export const runtime = isDev ? 'nodejs' : 'edge';
+export const runtime = 'edge';
 // This is required to enable streaming
 export const dynamic = 'force-dynamic';
 
