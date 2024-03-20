2fc005eea8c119045d91f6e3a3876af97afbf46b
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-03-20 20:58:15 +0100

    feat: run modification

--- apps/frontend/src/app/lib/services/assistant.ts
+++ apps/frontend/src/app/lib/services/assistant.ts
@@ -43,79 +43,100 @@ export const askAssistant = async (publicThreadId: string) => {
   const assistant = await openai.beta.assistants.retrieve(ASSISTANT_ID);
 
   // step: run the assistant
-  const run = await openai.beta.threads.runs.create(threadId, {
+  // without streaming
+  let run = await openai.beta.threads.runs.create(threadId, {
     assistant_id: assistant.id,
     // instructions: 'Co to jest sprzedaz b2b?', // this will override the default instructions of the Assistant
   });
 
+  // with streaming
+  // const run = openai.beta.threads.runs.createAndStream(thread.id, {
+  //   assistant_id: assistant.id
+  // })
+  //   .on('textCreated', (text) => process.stdout.write('\nassistant > '))
+  //   .on('textDelta', (textDelta, snapshot) => process.stdout.write(textDelta.value))
+  //   .on('toolCallCreated', (toolCall) => process.stdout.write(`\nassistant > ${toolCall.type}\n\n`))
+  //   .on('toolCallDelta', (toolCallDelta, snapshot) => {
+  //     if (toolCallDelta.type === 'code_interpreter') {
+  //       if (toolCallDelta.code_interpreter.input) {
+  //         process.stdout.write(toolCallDelta.code_interpreter.input);
+  //       }
+  //       if (toolCallDelta.code_interpreter.outputs) {
+  //         process.stdout.write("\noutput >\n");
+  //         toolCallDelta.code_interpreter.outputs.forEach(output => {
+  //           if (output.type === "logs") {
+  //             process.stdout.write(`\n${output.logs}\n`);
+  //           }
+  //         });
+  //       }
+  //     }
+  //   });
+
   const runId = run.id;
 
   // step: check the status
-  let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  run = await openai.beta.threads.runs.retrieve(threadId, runId);
 
   // Polling mechanism to see if runStatus is completed
   // TODO: this should be done more robust
-  while (runStatus.status !== 'completed') {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  while (['queued', 'in_progress', 'cancelling'].includes(run.status)) {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+    run = await openai.beta.threads.runs.retrieve(threadId, runId);
   }
 
   // step: check the answer
-  const messages = await openai.beta.threads.messages.list(threadId);
+  if (run.status === 'completed') {
+    const messages = await openai.beta.threads.messages.list(threadId);
 
-  const lastMessageForRun = messages.data
-    .filter(
-      (message) => message.run_id === runId && message.role === 'assistant'
-    )
-    .pop();
+    const lastMessageForRun = messages.data
+      .filter(
+        (message) => message.run_id === runId && message.role === 'assistant'
+      )
+      .pop();
 
-  // TODO: response
-  if (lastMessageForRun) {
-    const assistantMessageContent = parseThreadMessage(lastMessageForRun);
-    console.log(`${assistantMessageContent}`);
+    // TODO: response
+    if (lastMessageForRun) {
+      const assistantMessageContent = parseThreadMessage(lastMessageForRun);
+      console.log(`${assistantMessageContent}`);
 
-    const dbMessage = await createMessage({
-      thread: threadEntity,
-      message: {
-        id: lastMessageForRun.id,
-        created_at: lastMessageForRun.created_at,
-        content: assistantMessageContent,
-      },
-      role: Role.ASSISTANT,
-    });
-
-    const messageToSend: SseMessageEvent = {
-      type: 'message',
-      payload: {
-        public_id: dbMessage.public_id,
-        role: dbMessage.role,
-        created_at: dbMessage.created_at,
-        content: dbMessage.content,
-      },
-    };
-
-    const stringifiedMessage = JSON.stringify(messageToSend);
-
-    try {
-      const redis = new Redis({
-        url: process.env.REDIS_URL!,
-        token: process.env.REDIS_TOKEN!,
+      const dbMessage = await createMessage({
+        thread: threadEntity,
+        message: {
+          id: lastMessageForRun.id,
+          created_at: lastMessageForRun.created_at,
+          content: assistantMessageContent,
+        },
+        role: Role.ASSISTANT,
       });
 
-      await redis.publish(
-        `${redisChannelPrefix}-${publicThreadId}`,
-        stringifiedMessage
-      );
-    } catch (e) {
-      console.log('Redis publish error: ', e);
+      const messageToSend: SseMessageEvent = {
+        type: 'message',
+        payload: {
+          public_id: dbMessage.public_id,
+          role: dbMessage.role,
+          created_at: dbMessage.created_at,
+          content: dbMessage.content,
+        },
+      };
+
+      const stringifiedMessage = JSON.stringify(messageToSend);
+
+      try {
+        const redis = new Redis({
+          url: process.env.REDIS_URL!,
+          token: process.env.REDIS_TOKEN!,
+        });
+
+        await redis.publish(
+          `${redisChannelPrefix}-${publicThreadId}`, // mewa-123
+          stringifiedMessage
+        );
+      } catch (e) {
+        console.log('Redis publish error: ', e);
+      }
+
+      return true;
     }
-
-    // redis.set(
-    //   publicThreadId, // TODO: rather runId not thread?
-    //   stringifiedMessage
-    // );
-
-    return true;
   }
 
   throw new Error('Cannot fetch message from assistant');
