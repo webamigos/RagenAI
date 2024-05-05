17fcdd29e8f72688109aff952f06d4678eb89baa
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-05-05 15:46:21 +0200

    feat: implement shadcn

--- libs/common-ui/tailwind.config.js
+++ libs/common-ui/tailwind.config.js
@@ -1,30 +1,5 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const TailwindConfig = require('../ui-kit/utils/src/tailwind/tailwind.config');
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'salesyy-red': '#CB1D3D',
-        'salesyy-blue': '#252D53',
-      },
-    },
-  },
-  plugins: [],
+  ...TailwindConfig,
 };
