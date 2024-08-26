50cd411a2e893a8335b79cb14ad9f39809757e6f
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-08-26 11:56:47 +0200

    deleted console log

--- apps/frontend/src/app/api/visitor/[publicId]/route.ts
+++ apps/frontend/src/app/api/visitor/[publicId]/route.ts
@@ -16,7 +16,6 @@ export const GET = async (_request: Request, { params }: Params) => {
     const visitorMessages = await getLast24hVisitorMessages(publicId);
     return NextResponse.json({ messages: visitorMessages });
   } catch (error) {
-    console.error('Error during fetch visitor messages stats');
     return NextResponse.json({
       error: 'Error during fetch visitor messages stats',
       status: StatusCodes.BAD_REQUEST,
