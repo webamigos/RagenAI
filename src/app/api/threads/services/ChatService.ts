2989c802892f0fa61fd9353fc08317e23f4be682
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-06 14:37:22 +0200

    fix: supabase env attached

--- src/app/api/threads/services/ChatService.ts
+++ src/app/api/threads/services/ChatService.ts
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const apiKey = process.env.OPENAI_API_KEY;
 const model = process.env.OPENAI_CHAT_MODEL;
-const sbApiKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-const sbUrl = process.env.SUPABASE_URL!;
+const sbApiKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const sbUrl = process.env.SUPABASE_URL;
 
 export const createChatInstance = new ChatOpenAI({
   apiKey,
@@ -19,5 +19,9 @@ export const embeddingModel = new OpenAIEmbeddings({
   apiKey,
   model: 'text-embedding-ada-002',
 });
+
+if (!sbUrl || !sbApiKey) {
+  throw new Error('supabaseUrl is required.');
+}
 
 export const supeBaseClient = createClient(sbUrl, sbApiKey);
