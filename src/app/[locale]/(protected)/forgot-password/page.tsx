512fac345647d1fe3e4660ac620b7f8b7736b6d3
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-10 15:08:36 +0200

    fix: moving reset and forgot password to the correct folder

--- src/app/[locale]/(protected)/forgot-password/page.tsx
+++ src/app/[locale]/(protected)/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-import { ForgotPasswordForm } from '@/app/components/Forms/';
+import { ForgotPasswordForm } from '@/app/components/Forms/ForgotPasswordForm';
 
 export default function ForgotPasswordPage() {
   return <ForgotPasswordForm />;
