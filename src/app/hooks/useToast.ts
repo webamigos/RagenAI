8c85528a043368638a374732c844ae673b68a221
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-16 12:10:51 +0200

    fix: useToast

--- src/app/hooks/useToast.ts
+++ src/app/hooks/useToast.ts
@@ -1,30 +1,46 @@
 import { toast } from 'react-toastify';
 
+type ToastProps = {
+  message: string;
+  autoClose: number;
+  position:
+    | 'top-center'
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-center'
+    | 'bottom-left'
+    | 'bottom-right';
+};
+
 export const useToast = () => {
-  const successToast = (message: string) => {
+  const successToast = ({
+    message,
+    position,
+    autoClose = 3000,
+  }: ToastProps) => {
     toast.success(message, {
-      position: 'top-right',
-      autoClose: 3000,
+      position: position,
+      autoClose: autoClose,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
     });
   };
 
-  const errorToast = (message: string) => {
+  const errorToast = ({ message, position, autoClose = 3000 }: ToastProps) => {
     toast.error(message, {
-      position: 'top-right',
-      autoClose: 3000,
+      position: position,
+      autoClose: autoClose,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
     });
   };
 
-  const infoToast = (message: string) => {
+  const infoToast = ({ message, position, autoClose = 3000 }: ToastProps) => {
     toast.info(message, {
-      position: 'top-right',
-      autoClose: 3000,
+      position: position,
+      autoClose: autoClose,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
