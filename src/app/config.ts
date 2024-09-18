1b166097efab33ee2cd0d63b5695413345a1bd1d
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-18 16:13:06 +0200

    fix: using context to find answers

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,11 +3,11 @@ export const defaultLocale = 'en';
 export const redisChannelPrefix = 'assistant-messages-';
 export const dailyMessageLimit = 3;
 
-export const standaloneQuestionTemplate = `Given some conversation history (if any) and a question.
-conversation history: {conv_history}
-question: {question}`;
-export const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." Don't try to make up an answer. Always speak as if you were chatting to a friend.
+export const TEMPLATE = `Answer the user's questions based only on the following context. If the answer is not in the context, reply politely that you do not have that information available.:
+==============================
 context: {context}
-conversation history: {conv_history}
-question: {question}
-answer: `;
+==============================
+chat_history: {chat_history}
+
+user: {question}
+assistant:`;
