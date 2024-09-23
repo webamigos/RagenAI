4f57a9f1b778ba3062cfbb53191a3363bede7456
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-09-24 01:15:31 +0200

    fix: build

--- src/app/[locale]/(marketing)/redis-test/page.tsx
+++ src/app/[locale]/(marketing)/redis-test/page.tsx
@@ -1,27 +1,20 @@
-'use client';
+import { unstable_setRequestLocale as setRequestLocale } from 'next-intl/server';
+import Client from './Client';
 
-import { useState } from 'react';
-import { increase, decrease } from './actions';
-import { Button } from '@salesyy/common-ui/Button';
-
-export default function RedisTestPage() {
-  const [value, setValue] = useState(0);
-
-  const handleDecrease = async () => {
-    const result = await decrease();
-    setValue(result.counter);
+type Props = {
+  params: {
+    locale: string;
   };
+};
 
-  const handleIncrease = async () => {
-    const result = await increase();
-    setValue(result.counter);
-  };
+export const dynamic = 'force-dynamic';
+
+export default function RedisTestPage({ params: { locale } }: Props) {
+  setRequestLocale(locale);
 
   return (
     <div>
-      <Button label="Decrease" onClick={handleDecrease} className="mr-2" />
-      <span className="mr-2">Current value: {value}</span>
-      <Button label="Increase" onClick={handleIncrease} />
+      <Client />
     </div>
   );
 }
