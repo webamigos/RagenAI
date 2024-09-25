b30253269f4bd96ff53aa8bbeff7251f987836d8
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-25 10:34:25 +0200

    fix: supabase types

--- src/app/api/threads/services/ChatService.ts
+++ src/app/api/threads/services/ChatService.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from '@langchain/openai';
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/libs/db/supabase';
+import { DatabaseGenerated } from '@/libs/db/supabase';
 
 const apiKey = process.env.OPENAI_API_KEY;
 const model = process.env.OPENAI_CHAT_MODEL;
@@ -28,4 +28,4 @@ if (!sbUrl || !sbApiKey) {
   throw new Error('supabaseUrl is required.');
 }
 
-export const supaBaseClient = createClient<Database>(sbUrl, sbApiKey);
+export const supaBaseClient = createClient<DatabaseGenerated>(sbUrl, sbApiKey);
