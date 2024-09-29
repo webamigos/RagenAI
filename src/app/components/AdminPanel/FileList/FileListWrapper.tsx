56efc88b156183f427ef100e2707d0b27829c4da
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-09-29 09:20:50 +0200

    fix: removed asynchronous component

--- src/app/components/AdminPanel/FileList/FileListWrapper.tsx
+++ src/app/components/AdminPanel/FileList/FileListWrapper.tsx
@@ -1,28 +1,12 @@
 'use client';
 
-import { useUser } from '@clerk/nextjs';
-import { useEffect, useState } from 'react';
 import { FileList } from './FileList';
-import { SpinnerSVG } from '@salesyy/common-ui/icons';
 import { Card } from '@salesyy/common-ui/Card';
 
 export const FileListWrapper = () => {
-  const { user, isLoaded } = useUser();
-  const [userId, setUserId] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (isLoaded && user?.publicMetadata.visitorId) {
-      setUserId(user.publicMetadata.visitorId as string);
-    }
-  }, [isLoaded, user]);
-
-  if (!userId) {
-    return <SpinnerSVG />;
-  }
-
   return (
     <Card title="Lista plików" size="full">
-      <FileList userId={userId} />
+      <FileList />
     </Card>
   );
 };
