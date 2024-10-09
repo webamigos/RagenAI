adcefa77f0c73e0d0bc7e7278641f1d69e247966
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-10-09 11:15:45 +0200

    fix: rwd

--- src/app/components/MyProfile/ChatInstanceSettings/SetChatTemperature.tsx
+++ src/app/components/MyProfile/ChatInstanceSettings/SetChatTemperature.tsx
@@ -66,7 +66,11 @@ export const SetChatTemperature = () => {
   };
 
   return (
-    <Card title={t('set-temperature')} className="w-1/2">
+    <Card
+      title={t('set-temperature')}
+      size="full"
+      className="mb-5 md:mb-0 md:mr-5"
+    >
       <div className="flex items-center">
         <Input
           className="cursor-pointer"
