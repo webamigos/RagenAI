326dbfb9a3e56a43254c281c762113d2c34210b6
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-10-12 18:46:20 +0200

    feat: checking if threads are invisible after logging out

--- e2e/07-user-threads-history-visibility.spec.ts
+++ e2e/07-user-threads-history-visibility.spec.ts
@@ -1,23 +1,23 @@
 import { test, expect } from '@playwright/test';
+import { setupClerkTestingToken } from '@clerk/testing/playwright';
 
 test.setTimeout(30000);
 
 test.describe('User Threads History - Display Category When Threads Exist', () => {
   test.beforeEach(async ({ page }) => {
+    await setupClerkTestingToken({ page });
     await page.goto('/en/sign-in');
-
     await page.waitForSelector('input[name="email"]', { timeout: 20000 });
 
     const testEmail = process.env.TESTS_CLERK_USER_EMAIL!;
     const testPassword = process.env.TESTS_CLERK_USER_PASSWORD!;
 
     await page.getByRole('button', { name: 'Sign in' }).click();
-
     await page.locator('#email').fill(testEmail);
     await page.locator('#password').fill(testPassword);
     await page.click('button[type="submit"]');
-
     await page.waitForURL(/\/en/, { timeout: 10000 });
+
     let atLeastOneCategoryVisible = false;
     const categoriesEnglish = ['today', 'yesterday', 'older'];
 
---
+++ e2e/08-user-threads-history-visibility-after-logout.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+test.setTimeout(30000);
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/en');
+});
+
+test('should hide threads after user logs out', async ({ page }) => {
+  const testEmail = process.env.TESTS_CLERK_USER_EMAIL!;
+  const testPassword = process.env.TESTS_CLERK_USER_PASSWORD!;
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.locator('#email').fill(testEmail);
+  await page.locator('#password').fill(testPassword);
+  await page.click('button[type="submit"]');
+  await page.waitForTimeout(3000);
+
+  let atLeastOneCategoryVisible = false;
+  const categoriesEnglish = ['today', 'yesterday', 'older'];
+
+  for (const category of categoriesEnglish) {
+    const categoryLabel = page.getByText(category);
+    if (await categoryLabel.isVisible()) {
+      atLeastOneCategoryVisible = true;
+      break;
+    }
+  }
+
+  expect(atLeastOneCategoryVisible).toBe(true);
+
+  atLeastOneCategoryVisible = false;
+  await page.getByLabel('Dropdown menu').click();
+  await page.getByRole('menuitem', { name: 'Sign out' }).click();
+  await page.waitForTimeout(3000);
+
+  for (const category of categoriesEnglish) {
+    const categoryLabel = page.getByText(category);
+    if (await categoryLabel.isVisible()) {
+      atLeastOneCategoryVisible = true;
+      break;
+    }
+  }
+
+  expect(atLeastOneCategoryVisible).toBe(false);
+});
