63f6edfd3a41870ebf7ef97b36622dfab812ef84
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-10-18 12:21:42 +0200

    fix: prisma schema (#72)
    * fix: prisma schema
    
    * fix: restore database integrality
    
    * feat: db sync
    
    * feat: refactor documents types

--- src/context/DocumentsContext.tsx
+++ src/context/DocumentsContext.tsx
@@ -2,10 +2,10 @@ import { createContext, useState, useEffect } from 'react';
 import { useOrganization } from '@clerk/nextjs';
 
 import { getUserDocuments } from '@/app/actions';
-import { usersDocuments } from '@/app/contracts/Documents';
+import { type UsersDocuments } from '@/app/contracts/Documents';
 
 type DocumentsContextType = {
-  documents: usersDocuments[] | null;
+  documents: UsersDocuments[] | null;
   refreshDocuments: () => void;
   isLoading: boolean;
   isError: boolean;
@@ -20,7 +20,7 @@ export const DocumentsContext = createContext<DocumentsContextType | undefined>(
 );
 
 export const DocumentsProvider = ({ children }: Props) => {
-  const [documents, setDocuments] = useState<usersDocuments[] | null>(null);
+  const [documents, setDocuments] = useState<UsersDocuments[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
 
