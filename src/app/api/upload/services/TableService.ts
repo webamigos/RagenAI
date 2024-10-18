ab723eb889e553419328fd6b31374df14ab14b49
MJanowskiDev <mat.krz.janowski@gmail.com>
2024-10-18 23:33:30 +0200

    feat: vector store typegen script

--- src/app/api/upload/services/TableService.ts
+++ src/app/api/upload/services/TableService.ts
@@ -5,23 +5,10 @@ import { supabaseVectorStoreClient } from '@/libs/db/supabaseVectorStoreClient';
 //Todo remove unused parameter visitor_id
 export async function deleteDocument(visitor_id: string, document_id: string) {
   try {
-    const deleteEmbeddingsQuery = `
-      DELETE FROM ${VECTOR_STORE_TABLE_NAME}
-      WHERE metadata->>'document_id' = '${document_id}';
-    `;
-
-    const { error: deleteEmbeddingsError } =
-      await supabaseVectorStoreClient.rpc('execute_sql', {
-        sql_text: deleteEmbeddingsQuery,
-      });
-
-    if (deleteEmbeddingsError) {
-      logger.error(
-        `Error deleting embeddings for document ${document_id}:`,
-        deleteEmbeddingsError
-      );
-      throw deleteEmbeddingsError;
-    }
+    await supabaseVectorStoreClient
+      .from(VECTOR_STORE_TABLE_NAME)
+      .delete()
+      .eq('metadata->>document_id', document_id);
   } catch (error) {
     logger.error(`Error in deleteDocument function:`, error);
     throw error;
