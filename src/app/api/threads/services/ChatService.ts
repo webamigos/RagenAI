57d4cc966dec47e3ca8f930693b2b1d9c33d6132
MJanowskiDev <mat.krz.janowski@gmail.com>
2024-10-21 10:52:50 +0200

    feat: verbose mode only for development stage

--- src/app/api/threads/services/ChatService.ts
+++ src/app/api/threads/services/ChatService.ts
@@ -1,5 +1,4 @@
-import { ChatOpenAI } from '@langchain/openai';
-import { OpenAIEmbeddings } from '@langchain/openai';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 
 import {
   getModel,
@@ -27,7 +26,7 @@ export const createChatInstance = async (request: NextRequest) => {
     modelName,
     temperature,
     streaming: true,
-    verbose: true, //to be removed on prod
+    verbose: process.env.NODE_ENV === 'development',
   });
 };
 
