8488fd5bc48c9e6e44adde4a55b056d08fae6b6e
MJanowskiDev <91795381+MJanowskiDev@users.noreply.github.com>
2024-10-25 10:30:14 +0200

    Feat/langchain rag improvements (#79)
    * feat: wip implementing RAG chain using standalone question
    
    * refactor: store and stream only chain final answer
    
    * chore: add launch.json for Next.js debugging
    
    * feat: added support for filtering organization documents on vector search
    
    * feat: update table names and query names for vector store
    
    * refactor: remove unused chain
    
    * feat: thread conversation prompt engineering
    
    * feat: question answering chain improvements
    
    * fix: after merge
    
    * fix: user input cleanup removing diactricts
    
    * chore: refactor question answering chain
    
    * feat: makrdown text splitter tweaking
    
    * feat: rag model params tweak
    
    * fix: question answering chain types
    
    * refactor: moved basic rag into libs
    
    * refactor: remove model creation logic from basic rag chain
    
    * refactor: cleanup, removed old chain
    
    * refactor: type safety
    
    * fix: org_id capitalization mismatch
    
    * fix: error logging
    
    * refactor: added tsdoc for basic rag chain
    
    * feat: increase max retreived documents count

---
+++ src/libs/chains/types/basic-rag.ts
@@ -0,0 +1,27 @@
+import { BaseChatModel } from '@langchain/core/language_models/chat_models';
+import { RunnableBinding, RunnableConfig } from '@langchain/core/runnables';
+import { VectorStore } from '@langchain/core/vectorstores';
+import { BaseChain } from 'langchain/chains';
+
+export interface BasicRagChainInput {
+  question: string;
+  chat_history: string | undefined;
+}
+
+export interface BasicRagChainParams {
+  vectorStore: VectorStore;
+  models: {
+    contentModerator: BaseChain;
+    questionRephraser: BaseChatModel;
+    answerGenerator: BaseChatModel;
+  };
+}
+
+export interface BasicRagChainOutput {
+  chain: RunnableBinding<
+    BasicRagChainInput,
+    string,
+    RunnableConfig<Record<string, any>>
+  >;
+  finalAnswerRunName: string;
+}
