f2d108484ba8fe1e44ed2efda69cac6f34606110
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-10-28 16:58:50 +0100

    fix: clerk custom component's content width

--- src/app/components/MyProfile/OrganizationProfile/ManageOrganization.tsx
+++ src/app/components/MyProfile/OrganizationProfile/ManageOrganization.tsx
@@ -9,27 +9,31 @@ export const ManageOrganization = () => {
   const { resolvedTheme } = useTheme();
 
   return (
-    <Card className="min-w-max p-0" size="full">
-      <OrganizationProfile
-        appearance={{
-          baseTheme: resolvedTheme === 'dark' ? dark : experimental__simple,
-          variables: {
-            colorBackground: resolvedTheme === 'dark' ? '#253745' : '',
-            colorInputBackground: resolvedTheme === 'dark' ? '#253745' : '',
-            colorText: resolvedTheme === 'dark' ? '#e5e7eb' : '',
-          },
-          elements: {
-            cardBox: 'grid-cols-1 h-1/2 w-full shadow-none border-none',
-            actionCard: 'dark:bg-secondary-dark',
-            navbar: 'hidden',
-            footer: 'hidden',
-            pageScrollBox:
-              'flex w-full flex-row bg-white dark:bg-secondary-dark border-none',
-            scrollBox: 'bg-white dark:bg-secondary-dark border-none',
-            navbarMobileMenuRow: 'hidden',
-          },
-        }}
-      />
+    <Card className="w-full p-0" size="full">
+      <div className="w-full overflow-x-auto">
+        <OrganizationProfile
+          appearance={{
+            baseTheme: resolvedTheme === 'dark' ? dark : experimental__simple,
+            variables: {
+              colorBackground: resolvedTheme === 'dark' ? '#253745' : '',
+              colorInputBackground: resolvedTheme === 'dark' ? '#253745' : '',
+              colorText: resolvedTheme === 'dark' ? '#e5e7eb' : '',
+            },
+            elements: {
+              cardBox: 'grid-cols-1 h-1/2 shadow-none border-none',
+              actionCard: 'dark:bg-secondary-dark',
+              navbar: 'hidden',
+              footer: 'hidden',
+              pageScrollBox:
+                'flex w-full flex-col bg-white dark:bg-secondary-dark border-none',
+              scrollBox:
+                'flex w-full bg-white dark:bg-secondary-dark border-none',
+              navbarMobileMenuRow: 'hidden',
+              table: 'w-full !important!',
+            },
+          }}
+        />
+      </div>
     </Card>
   );
 };
