2c57b5cd0e6277d60f129f02665a64983620beaf
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-06 11:25:33 +0100

    feat: new target env variable differentiate envs: local, test, staging, production

--- src/app/lib/utils/logger/serverLogger.ts
+++ src/app/lib/utils/logger/serverLogger.ts
@@ -1,7 +1,7 @@
 import pino from 'pino';
 import { createWriteStream } from 'pino-sentry';
 
-import { isProduction } from '@/libs/utils/env';
+import { isProductionTargetEnv, isStagingTargetEnv } from '@/libs/utils/env';
 
 const streams = [];
 
@@ -10,11 +10,13 @@ if (typeof window !== 'undefined') {
   throw new Error('This module should only be used on the server side');
 }
 
-if (isProduction && process.env.SENTRY_DSN) {
+if ((isProductionTargetEnv || isStagingTargetEnv) && process.env.SENTRY_DSN) {
   const sentryStream = createWriteStream({
     dsn: process.env.SENTRY_DSN,
     level: 'info',
-    environment: process.env.NODE_ENV,
+    environment: process.env.TARGET_ENV
+      ? process.env.TARGET_ENV
+      : process.env.NODE_ENV,
   });
 
   streams.push({
