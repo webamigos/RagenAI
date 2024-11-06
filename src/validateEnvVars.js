4a427ec4f3986d13df3cd5f96d18b54085323689
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-06 13:33:01 +0100

    feat: env vars valiation

---
+++ src/validateEnvVars.js
@@ -0,0 +1,34 @@
+const { z } = require('zod');
+
+// TODO: use this schema instead process.env as source of truth?
+const envSchema = z.object({
+  // Supabase for the App
+  DATABASE_URL: z.string().url(),
+  DATABASE_DIRECT_URL: z.string().url(),
+
+  // Supabase for Vector store
+  SUPABASE_URL: z.string().url(),
+  SUPABASE_ANON_KEY: z.string(),
+
+  // Clerk
+  CLERK_SECRET_KEY: z.string(),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
+
+  // Langsmith?
+  LANGCHAIN_API_KEY: z.string(),
+  LANGCHAIN_TRACING_V2: z.coerce.boolean(),
+  LANGCHAIN_CALLBACKS_BACKGROUND: z.coerce.boolean(),
+  LANGCHAIN_ENDPOINT: z.string().url(),
+
+  // Redis for organization settings
+  REDIS_URL: z.string().url(),
+
+  // Target env
+  TARGET_ENV: z.enum(['local', 'test', 'staging', 'production']),
+});
+
+const validateEnvs = () => envSchema.safeParse(process.env);
+
+module.exports = validateEnvs;
