4606cc955cafa9c88106f682577d9cb365a25df8
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-15 15:53:16 +0100

    feat: separate file for jest to not use js files during transformation

--- jest.config.ts
+++ jest.config.ts
@@ -179,7 +179,7 @@ const config: Config = {
     '^.+\\.(ts|tsx)$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.json',
+        tsconfig: 'tsconfig.jest.json',
         jsx: 'react',
       },
     ],
