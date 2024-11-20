ae2e10c8aa2d31e7812b34b1e9ef0b6d796fb1dc
Mateusz Janowski <m.janowski@tubics.com>
2024-11-20 16:50:48 +0100

    feat: add api key modal

---
+++ src/app/components/ApiKeys/CreateApiKeyForm/ApiKeyModal.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useTranslations } from 'next-intl';
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogTitle,
+  Input,
+} from '@salesyy/common-ui';
+import { Clipboard, ClipboardChecked } from '@salesyy/common-ui/icons';
+
+type Props = {
+  isOpen: boolean;
+  apiKey: string;
+  onClose: () => void;
+};
+
+export const ApiKeyModal = ({ isOpen, apiKey, onClose }: Props) => {
+  const t = useTranslations('api-keys');
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(apiKey);
+    setIsCopied(true);
+    toast.success(t('dialog.api-key-generated.copied'));
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+
+  return (
+    <Dialog onClose={onClose} size="lg" open={isOpen}>
+      <DialogTitle>{t('dialog.api-key-generated.title')}</DialogTitle>
+      <DialogBody>
+        <div className="space-y-4 w-full">
+          <p className="text-sm text-gray-600">
+            {t('dialog.api-key-generated.description')}
+          </p>
+          <div className="flex items-end gap-2">
+            <div className="flex-1">
+              <Input value={apiKey} readOnly className="font-mono p-2" />
+            </div>
+            <Button
+              onClick={handleCopy}
+              iconRight={isCopied ? <ClipboardChecked /> : <Clipboard />}
+              label={t('copy')}
+            />
+          </div>
+        </div>
+      </DialogBody>
+      <DialogActions>
+        <button
+          onClick={onClose}
+          className="bg-transparent text-primary-blue-500 shadow-none border-primary-blue-500 pr-2"
+        >
+          {t('done')}
+        </button>
+      </DialogActions>
+    </Dialog>
+  );
+};
