80d5614227b3efd90255e8ffa056d77661fc1748
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-11-20 19:48:02 +0100

    feat: add dynamic code block wrappers and styling with Highlight.js in useChatViewLogic

--- src/app/components/Assistant/ChatOutput/useChatViewLogic.ts
+++ src/app/components/Assistant/ChatOutput/useChatViewLogic.ts
@@ -1,7 +1,9 @@
 import { useUser } from '@clerk/nextjs';
 import { useState, useEffect } from 'react';
 import { useTranslations } from 'next-intl';
-import markdownit from 'markdown-it';
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github-dark.css';
 
 import type { StreamedMessageDto } from '@/app/contracts/Message';
 
@@ -14,7 +16,23 @@ export const useChatViewLogic = (
   const { user } = useUser();
   const userAvatar = user?.imageUrl;
   const t = useTranslations('chat');
-  const md = markdownit();
+
+  const md = new MarkdownIt({
+    highlight: (code, lang) => {
+      let highlightedCode;
+
+      if (lang && hljs.getLanguage(lang)) {
+        highlightedCode = hljs.highlight(code, { language: lang }).value;
+      } else {
+        highlightedCode = hljs.highlightAuto(code).value;
+      }
+      return `
+      <div class="code-wrapper">
+        <pre class="hljs"><code>${highlightedCode}</code></pre>
+      </div>
+    `;
+    },
+  });
 
   useEffect(() => {
     if (streamedMessage) {
