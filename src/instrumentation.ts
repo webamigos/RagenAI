19db9a4972921db21e45b7ac84a23714b23e2b76
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-21 16:15:56 +0100

    fix: instrumentation only for staging and production target env

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,14 @@
 import * as Sentry from '@sentry/nextjs';
 
+const TARGET_ENV = process.env.TARGET_ENV;
+const isProductionTarget = 'production';
+const isStagingTarget = 'staging';
+
 export async function register() {
+  if (!(isProductionTarget || isStagingTarget)) {
+    return;
+  }
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config');
   }
