66fe8c85e36a3f00d931b949fdb31ac989b265c6
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-11-21 14:22:53 +0100

    refactor: code cleanup

--- src/app/api/threads/services/initializeBasicRag.ts
+++ src/app/api/threads/services/initializeBasicRag.ts
@@ -4,7 +4,6 @@ import { supabaseVectorStoreClient } from '@/libs/db/supabaseVectorStoreClient';
 import { VectorStoreMetadataFilter } from '@/app/lib/types/types';
 import { OrganizationSettings } from '@/app/lib/types/settings';
 import { basicRagChain } from '@/libs/chains/basic-rag/chain';
-import { coderChain } from '@/libs/chains/coder/chain';
 import { DOCUMENT_SEARCH_QUERY_NAME } from '@/libs/db/constants/vectorStore';
 import { Embeddings } from '@langchain/core/embeddings';
 import { SupabaseClient } from '@supabase/supabase-js';
@@ -67,7 +66,7 @@ export const initializeRagChain = ({ settings }: InitializeRagChainParams) => {
       embeddingModel
     );
 
-    return coderChain({
+    return basicRagChain({
       models: {
         contentModerator,
         questionRephraser,
