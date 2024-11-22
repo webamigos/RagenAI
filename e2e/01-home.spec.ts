c1604ac5597e2b1d151ca6ebdf8c8e98845a1ad3
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-22 11:38:13 +0100

    fix: e2e tests

--- e2e/01-home.spec.ts
+++ e2e/01-home.spec.ts
@@ -5,8 +5,9 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('home elements are visible', async ({ page }) => {
-  await expect(page.getByText(/set-up checklist/i)).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
+
   await expect(
     page.getByRole('button', { name: /start new thread/i })
-  ).not.toBeVisible();
+  ).toBeVisible();
 });
