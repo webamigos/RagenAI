3583cae3f7a03ae252a64a31c328176cc191bdc8
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-28 23:54:53 +0100

    fix: test with imports

--- src/app/api/run-workflow/route.ts
+++ src/app/api/run-workflow/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { getTemporalClient } from '@/temporal/src/client';
-import { EmbeddingWorkflow } from '@/temporal/src/workflows';
+import { getTemporalClient } from '../../../temporal/src/client';
+import { EmbeddingWorkflow } from '../../../temporal/src/workflows';
 import {
   ACTIVITY_CANCEL_EMBEDDING_COMMAND,
   ACTIVITY_EMBEDDING_STATE_QUERY,
