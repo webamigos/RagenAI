5959f60c0f944b142889cce2a18c9060b349a85a
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-12-03 17:02:52 +0100

    refactor: react hook form for content form managing

--- src/app/[locale]/document/[id]/documentReducer.ts
+++ src/app/[locale]/document/[id]/documentReducer.ts
@@ -7,64 +7,45 @@ export const SET_DOCUMENT_TITLE = 'SET_DOCUMENT_TITLE';
 export const EDIT_TITLE_MODE = 'EDIT_TITLE_MODE';
 
 export type State = {
-  documentContent: string | null;
-  documentTitle: string | null;
   isLoading: boolean;
   isEditing: boolean;
-  editableContent: string | null;
   isEditingTitle: boolean;
-  editableTitle: string | null;
   isSaving: boolean;
+  documentContent: string;
+  documentTitle: string;
 };
 
 export type Action =
-  | { type: typeof SET_DOCUMENT; payload: { content: string; title: string } }
-  | { type: typeof SET_EDITING; payload: boolean }
-  | { type: typeof SET_LOADING; payload: boolean }
-  | { type: typeof SET_EDITABLE_CONTENT; payload: string }
-  | { type: typeof SET_SAVING; payload: boolean }
-  | { type: typeof SET_DOCUMENT_TITLE; payload: string }
-  | {
-      type: typeof EDIT_TITLE_MODE;
-      payload: { isEditing: boolean; title: string | null };
-    };
+  | { type: 'SET_IS_LOADING'; payload: boolean }
+  | { type: 'SET_IS_EDITING'; payload: boolean }
+  | { type: 'SET_IS_EDITING_TITLE'; payload: boolean }
+  | { type: 'SET_IS_SAVING'; payload: boolean }
+  | { type: 'SET_DOCUMENT_CONTENT'; payload: string }
+  | { type: 'SET_DOCUMENT_TITLE'; payload: string };
 
 export const initialState: State = {
-  documentContent: null,
-  documentTitle: null,
   isLoading: true,
   isEditing: false,
-  editableContent: null,
   isEditingTitle: false,
-  editableTitle: null,
   isSaving: false,
+  documentContent: '',
+  documentTitle: '',
 };
 
 export function reducer(state: State, action: Action): State {
   switch (action.type) {
-    case SET_DOCUMENT:
-      return {
-        ...state,
-        documentContent: action.payload.content,
-        documentTitle: action.payload.title,
-        isLoading: false,
-      };
-    case SET_EDITING:
-      return { ...state, isEditing: action.payload };
-    case SET_LOADING:
+    case 'SET_IS_LOADING':
       return { ...state, isLoading: action.payload };
-    case SET_EDITABLE_CONTENT:
-      return { ...state, editableContent: action.payload };
-    case SET_SAVING:
+    case 'SET_IS_EDITING':
+      return { ...state, isEditing: action.payload };
+    case 'SET_IS_EDITING_TITLE':
+      return { ...state, isEditingTitle: action.payload };
+    case 'SET_IS_SAVING':
       return { ...state, isSaving: action.payload };
-    case SET_DOCUMENT_TITLE:
+    case 'SET_DOCUMENT_CONTENT':
+      return { ...state, documentContent: action.payload };
+    case 'SET_DOCUMENT_TITLE':
       return { ...state, documentTitle: action.payload };
-    case EDIT_TITLE_MODE:
-      return {
-        ...state,
-        isEditingTitle: action.payload.isEditing,
-        editableTitle: action.payload.title,
-      };
     default:
       return state;
   }
