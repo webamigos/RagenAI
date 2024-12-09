34aac50b2759e892860d65a29814e20e57075a92
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2024-12-09 17:52:49 +0100

    fix: after error

--- src/app/components/UserLinks/UserLinks.tsx
+++ src/app/components/UserLinks/UserLinks.tsx
@@ -14,24 +14,22 @@ export const UserLinks = () => {
   const t = useTranslations();
 
   return (
-    <div>
+    <>
       {isSignedIn ? (
-        <div>
-          <SignOutButton>
-            <button className="flex font-sans text-sm font-semibold text-gray-500 hover:text-gray-600 dark:text-slate-200 dark:hover:text-white cursor-pointer">
-              <LogoutIcon className="mr-3" />
-              {t('common.sign-out')}
-            </button>
-          </SignOutButton>
-        </div>
+        <SignOutButton>
+          <span className="flex font-sans text-sm font-semibold text-gray-500 hover:text-gray-600 dark:text-slate-200 dark:hover:text-white cursor-pointer">
+            <LogoutIcon className="mr-3" />
+            {t('common.sign-out')}
+          </span>
+        </SignOutButton>
       ) : (
-        <SignInButton mode="redirect" aria-label="Open sign in">
+        <SignInButton mode="redirect">
           <span className="flex text-sm dark:text-slate-200 text-slate-900 dark:hover:text-white cursor-pointer">
-            <UserCircleIcon className="mr-4" />
+            <UserCircleIcon className="mr-3" />
             {t('common.sign-in')}
           </span>
         </SignInButton>
       )}
-    </div>
+    </>
   );
 };
