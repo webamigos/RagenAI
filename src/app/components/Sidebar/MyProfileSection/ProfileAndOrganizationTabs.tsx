63c21ba3f0608f15a7d33f4c452c2797364950f3
Mateusz Janowski <m.janowski@tubics.com>
2024-12-22 17:55:05 +0100

    feat: add manage subscription link to the nav

--- src/app/components/Sidebar/MyProfileSection/ProfileAndOrganizationTabs.tsx
+++ src/app/components/Sidebar/MyProfileSection/ProfileAndOrganizationTabs.tsx
@@ -13,6 +13,7 @@ import {
   OpenBookIcon,
   SettingsIcon,
   SidebarItem,
+  CreditCardIcon,
 } from '@ragenai/common-ui';
 
 import { OrganizationRoles } from '@/app/contracts/User';
@@ -66,6 +67,11 @@ export const ProfileAndOrganizationTabs = ({ membership }: Props) => {
       icon: SettingsIcon,
       label: t('assistant-management'),
       path: '/my-profile/prompt-management',
+    },
+    {
+      icon: CreditCardIcon,
+      label: t('subsciption-management'),
+      path: '/my-profile/subscription',
     },
   ];
 
--- src/libs/common-ui/icons/index.ts
+++ src/libs/common-ui/icons/index.ts
@@ -41,3 +41,4 @@ export { KeyIcon } from './KeyIcon';
 export { ClourArrowIcon } from './CloudArrowIcon';
 export { CheckIcon } from './CheckIcon';
 export { SearchIcon } from './SearchIcon';
+export { CreditCardIcon } from './CreditCardIcon';
