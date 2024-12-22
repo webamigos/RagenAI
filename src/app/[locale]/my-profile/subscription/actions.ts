09c3580b42ba6bbf1085ee9a2c31f4a5298bcca0
Mateusz Janowski <m.janowski@tubics.com>
2024-12-22 18:42:16 +0100

    feat: subscription canceling

--- src/app/[locale]/my-profile/subscription/actions.ts
+++ src/app/[locale]/my-profile/subscription/actions.ts
@@ -5,6 +5,8 @@ import { auth } from '@clerk/nextjs/server';
 import db from '@ragenai/prisma-client';
 
 import type { SubscriptionDetails } from './types';
+import { stripe } from '@/libs/payments/stripe';
+import Stripe from 'stripe';
 
 export async function getSubscriptionData(): Promise<SubscriptionDetails | null> {
   const { orgId } = auth();
@@ -27,4 +29,18 @@ export async function getSubscriptionData(): Promise<SubscriptionDetails | null>
   });
 
   return organization?.subscription ?? null;
+}
+
+export async function cancelSubscription(
+  subscriptionId: string | null
+): Promise<Stripe.Response<Stripe.Subscription> | null> {
+  const { orgId } = auth();
+
+  if (!orgId || !subscriptionId) {
+    return null;
+  }
+
+  return stripe.subscriptions.update(subscriptionId, {
+    cancel_at_period_end: true,
+  });
 }
--- src/app/[locale]/my-profile/subscription/components/SubscribtionInfo.tsx
+++ src/app/[locale]/my-profile/subscription/components/SubscribtionInfo.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { format } from 'date-fns';
 import { useTranslations } from 'next-intl';
 import type { SubscriptionDetails } from '../types';
-import { SubscriptionStatus } from '@prisma/client';
+import { PlanType, SubscriptionStatus } from '@prisma/client';
+import { Button, Link } from '@ragenai/common-ui';
+import { cancelSubscription } from '../actions';
+import { toast } from 'react-toastify';
 
 type Props = {
   subscription: SubscriptionDetails;
@@ -15,9 +20,20 @@ export const SubscriptionInfo = ({
     current_period_end,
     current_period_start,
     canceled_at,
+    stripe_subscription_id,
   },
 }: Props) => {
   const t = useTranslations('subscription');
+
+  const handleCancelSubscription = async () => {
+    const response = await cancelSubscription(stripe_subscription_id);
+    if (response) {
+      toast.success(t('cancel-subscription-success'));
+    } else {
+      toast.error(t('cancel-subscription-error'));
+    }
+  };
+
   return (
     <div className="space-y-4 mt-4">
       <div>
@@ -28,7 +44,6 @@ export const SubscriptionInfo = ({
           {plan.name} ({plan.type})
         </p>
       </div>
-
       <div>
         <h2 className="text-sm font-medium text-gray-500">{t('status')}</h2>
         <p className="mt-1 text-lg font-semibold capitalize">
@@ -43,7 +58,6 @@ export const SubscriptionInfo = ({
           </span>
         </p>
       </div>
-
       <div>
         <h2 className="text-sm font-medium text-gray-500">
           {t('period-start')}
@@ -52,7 +66,6 @@ export const SubscriptionInfo = ({
           {format(current_period_start, 'dd.MM.yyyy')}
         </p>
       </div>
-
       {canceled_at && (
         <div>
           <h2 className="text-sm font-medium text-gray-500">
@@ -61,9 +74,9 @@ export const SubscriptionInfo = ({
           <p className="mt-1 text-lg font-semibold">
             {format(canceled_at, 'dd.MM.yyyy')}
           </p>
+          <p className="text-sm">{t('cancel-subscription-confirmation')}</p>
         </div>
       )}
-
       {trial_end && (
         <div>
           <h2 className="text-sm font-medium text-gray-500">
@@ -74,7 +87,6 @@ export const SubscriptionInfo = ({
           </p>
         </div>
       )}
-
       {current_period_end && (
         <div>
           <h2 className="text-sm font-medium text-gray-500">
@@ -85,6 +97,22 @@ export const SubscriptionInfo = ({
           </p>
         </div>
       )}
+
+      <div className="flex gap-4 items-center">
+        {/* Todo: add confirmation modal */}
+        {plan.type === PlanType.STRIPE && !canceled_at && (
+          <Button
+            className="bg-red-500 hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700"
+            onClick={handleCancelSubscription}
+          >
+            {t('cancel-subscription')}
+          </Button>
+        )}
+
+        {plan.type === PlanType.INTERNAL || canceled_at ? (
+          <Link href="/plans">{t('show-available-plans')}</Link>
+        ) : null}
+      </div>
     </div>
   );
 };
