7fd7bded6837135d3c2d9447f0e4f5e8c8d2b368
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-14 14:41:49 +0100

    feat: qdrant with unstructured.io

--- src/app/api/threads/services/saveDataInVectorTable.ts
+++ src/app/api/threads/services/saveDataInVectorTable.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
 import { EPubLoader } from '@langchain/community/document_loaders/fs/epub';
+import { UnstructuredLoader } from '@langchain/community/document_loaders/fs/unstructured';
 import { Document } from 'langchain/document';
 import { MarkdownTextSplitter } from 'langchain/text_splitter';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
@@ -50,11 +51,18 @@ const CHUNK_SETTINGS = {
     chunkSize: 1500,
     chunkOverlap: 250,
   },
+  pdf: {
+    chunkSize: 1000,
+    chunkOverlap: 250,
+  },
 } as const;
 
-const saveBinaryToTempFile = async (content: string | Buffer) => {
+const saveBinaryToTempFile = async (
+  content: string | Buffer,
+  extension: string
+) => {
   const projectDir = process.cwd();
-  const filePath = path.join(projectDir, `temp-${Date.now()}.epub`);
+  const filePath = path.join(projectDir, `tmp/${Date.now()}.${extension}`);
 
   try {
     setSentryServiceTag(serviceName);
@@ -106,16 +114,34 @@ export const convertAndStoreDocument = async ({
     }
 
     const embeddingModel = await createEmbeddingsInstance({ apiKey });
+    const fileExtension = path.extname(fileName).slice(1).toLowerCase();
 
-    if (fileName.endsWith('.epub')) {
-      const { filePath, message } = await saveBinaryToTempFile(fileContent);
+    if (
+      fileExtension === 'pdf' ||
+      fileExtension === 'epub' ||
+      fileExtension === 'csv'
+    ) {
+      const { filePath, message, success } = await saveBinaryToTempFile(
+        fileContent,
+        fileExtension
+      );
+      if (!success || !filePath) {
+        return {
+          success: false,
+          message: `Failed to save temporary file: ${message}`,
+        };
+      }
+
       if (filePath) {
         try {
           await fs.promises.access(filePath, fs.constants.R_OK);
-          const load = new EPubLoader(filePath);
+          const load = new UnstructuredLoader(filePath, {
+            apiKey: process.env.UNSTRUCTURED_API_KEY,
+            apiUrl: process.env.UNSTRUCTURED_API_URL,
+          });
           rawDocs = await load.load();
         } catch (error) {
-          logger.error({ err: error }, 'Error loading EPub file');
+          logger.error({ err: error }, 'Error loading file');
           return {
             success: false,
             message: `File is not accessible at: ${filePath}`,
@@ -124,23 +150,43 @@ export const convertAndStoreDocument = async ({
         } finally {
           await fs.promises.unlink(filePath);
         }
-      } else {
-        return {
-          success: false,
-          message: `Failed to save temporary file: ${message}`,
-        };
-      }
-    } else {
-      const fileContentIsString = typeof fileContent === 'string';
-      if (fileContentIsString) {
-        rawDocs = [new Document({ pageContent: fileContent })];
-      } else {
-        return {
-          success: false,
-          message: 'Invalid file type detected.',
-        };
       }
     }
+
+    // if (fileName.endsWith('.epub')) {
+    //   const { filePath, message } = await saveBinaryToTempFile(fileContent);
+    //   if (filePath) {
+    //     try {
+    //       await fs.promises.access(filePath, fs.constants.R_OK);
+    //       const load = new EPubLoader(filePath);
+    //       rawDocs = await load.load();
+    //     } catch (error) {
+    //       logger.error({ err: error }, 'Error loading EPub file');
+    //       return {
+    //         success: false,
+    //         message: `File is not accessible at: ${filePath}`,
+    //         error: error as Error,
+    //       };
+    //     } finally {
+    //       await fs.promises.unlink(filePath);
+    //     }
+    //   } else {
+    //     return {
+    //       success: false,
+    //       message: `Failed to save temporary file: ${message}`,
+    //     };
+    //   }
+    // } else {
+    //   const fileContentIsString = typeof fileContent === 'string';
+    //   if (fileContentIsString) {
+    //     rawDocs = [new Document({ pageContent: fileContent })];
+    //   } else {
+    //     return {
+    //       success: false,
+    //       message: 'Invalid file type detected.',
+    //     };
+    //   }
+    // }
     const textSplitterEPub = new RecursiveCharacterTextSplitter({
       chunkSize: CHUNK_SETTINGS.epub.chunkSize,
       chunkOverlap: CHUNK_SETTINGS.epub.chunkOverlap,
