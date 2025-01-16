8a93adeace2ddbcc08ff8ac74585a53935947b85
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2025-01-16 11:55:06 +0100

    fix: generate access key layout

--- src/app/[locale]/(marketing)/generate-access-key/components/generate-access-key.tsx
+++ src/app/[locale]/(marketing)/generate-access-key/components/generate-access-key.tsx
@@ -41,7 +41,7 @@ export const GenerateAccessKey = ({ organizationRecord }: Props) => {
     return null;
   }
 
-  const handleGenerateKey = async () => {
+  const handleGenerateEmbedId = async () => {
     const key = await generateKey(organizationClerkId);
     setKey(key);
   };
@@ -51,7 +51,7 @@ export const GenerateAccessKey = ({ organizationRecord }: Props) => {
       const decodedKey = await decodeKey(userKey);
       setDecodedKey(decodedKey);
     } catch (error) {
-      errorToast({ message: 'Klucz jest nieprawidłowy' });
+      errorToast({ message: t('decode-key-error') });
     }
   };
 
@@ -59,102 +59,140 @@ export const GenerateAccessKey = ({ organizationRecord }: Props) => {
     setIsOrgIdVisible((prev) => !prev);
   };
 
-  return (
-    <div className="space-y-6">
-      <Card title={t('title')} size="full" className="relative mt-[4.9rem]">
-        <div className="p-4 bg-gray-50 rounded-lg overflow-x-auto whitespace-pre-wrap break-all">
-          <div className="mb-4 text-sm text-gray-500">
-            <Text className="mb-1" fontWeight="bold">
-              {t('organization-id')}
-            </Text>
-            <div className="flex p-1 border rounded-lg w-full md:w-1/2 lg:w-[30%] items-center">
-              <Text className={`${isOrgIdVisible ? '' : 'blur-sm'} mr-auto`}>
-                {organizationClerkId}
-              </Text>
-              {isOrgIdVisible ? (
-                <EyeOffIcon
-                  className="w-4 h-4"
-                  onClick={toggleOrgIdVisibility}
-                />
-              ) : (
-                <OpenEyeIcon
-                  className="w-4 h-4"
-                  onClick={toggleOrgIdVisibility}
-                />
-              )}
-            </div>
-          </div>
-          <div className="text-sm text-gray-500">
-            <Text className="mb-1" fontWeight="bold">
-              {t('public-id')}
-            </Text>
-            <div className="flex p-1 border rounded-lg w-full md:w-1/2 lg:w-[30%] items-center">
-              <Text>{organizationPublicId}</Text>
-            </div>
-          </div>
-        </div>
-      </Card>
+  const embedScript = `<script src='${
+    window.location.origin
+  }/api/embed/${key}?${new URLSearchParams({
+    title: chatbotTitle,
+    message: chatbotName,
+  }).toString()}'></script>`;
+  const publicUrl = `${window.location.origin}/pl/public/${key}`;
 
-      {/* Configuration and Key Generation */}
-      <div>
-        <div className="flex flex-col items-start gap-4">
-          <h2 className="text-lg font-bold">Konfiguracja chatbota</h2>
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="w-full">
+        <Card
+          title={t('title')}
+          size="full"
+          className="mt-5 lg:mt-[4.9rem] mb-3"
+        >
+          <div className="p-4 bg-gray-50 rounded-lg whitespace-pre-wrap break-all">
+            <div className="text-sm text-gray-500">
+              <Text className="mb-1" fontWeight="bold">
+                {t('organization-id')}
+              </Text>
+              <div className="grid grid-cols-[1fr_auto] gap-2 p-1 border rounded-lg items-center">
+                <Text className={`${isOrgIdVisible ? '' : 'blur-sm'}`}>
+                  {organizationClerkId}
+                </Text>
+                {isOrgIdVisible ? (
+                  <EyeOffIcon
+                    className="w-4 h-4"
+                    onClick={toggleOrgIdVisibility}
+                  />
+                ) : (
+                  <OpenEyeIcon
+                    className="w-4 h-4"
+                    onClick={toggleOrgIdVisibility}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="text-sm text-gray-500">
+              <Text className="mb-1" fontWeight="bold">
+                {t('public-id')}
+              </Text>
+              <div className="p-1 border rounded-lg">
+                <Text>{organizationPublicId}</Text>
+              </div>
+            </div>
+          </div>
+        </Card>
+
+        <Card size="full" title={t('decode-key')}>
+          <div className="space-y-2">
+            <Input
+              value={userKey}
+              onChange={(e) => setUserKey(e.target.value)}
+              placeholder={t('decode-key-placeholder')}
+              className="max-w-md h-10 mb-2"
+            />
+            <Button onClick={handleDecodeKey}>{t('decode-key')}</Button>
+          </div>
+
+          {decodedKey && (
+            <div className="p-4 bg-gray-50 rounded-lg">
+              <p className="text-sm text-gray-600">
+                {t('decode-key')}:{' '}
+                <span className="font-mono text-lg text-red-500">
+                  {decodedKey}
+                </span>
+              </p>
+            </div>
+          )}
+        </Card>
+      </div>
+
+      <Card
+        size="full"
+        title={t('configure-public-access')}
+        className="mt-5 lg:mt-[4.9rem] h-min"
+      >
+        <div>
           <Input
             value={chatbotTitle}
             onChange={(e) => setChatbotTitle(e.target.value)}
-            placeholder="Tytuł okna czatbota"
+            placeholder={t('chatbot-title')}
             className="max-w-md h-10"
           />
           <Input
             value={chatbotName}
             onChange={(e) => setChatbotName(e.target.value)}
-            placeholder="Podtytuł okna czatbota"
-            className="max-w-md h-10"
+            placeholder={t('chatbot-subtitle')}
+            className="max-w-md h-10 mb-4"
           />
-          <Button onClick={handleGenerateKey}>Generuj klucz</Button>
-        </div>
+          <Button onClick={handleGenerateEmbedId}>
+            {t('generate-public-key')}
+          </Button>
 
-        {key && (
-          <div className="space-y-4 mt-6">
-            <div className="space-y-2 p-4 bg-gray-50 rounded-lg overflow-x-auto whitespace-pre-wrap break-all">
-              <p className="text-sm text-gray-600">
-                Twój klucz dostępu to:
-                <span className="block mt-1 font-mono text-lg text-red-500">
-                  {key}
-                </span>
-              </p>
-              <p className="text-sm text-orange-500">
-                Osoby z tym kluczem mają dostęp do wszystkich dokumentów w
-                organizacji.
-              </p>
+          {key && (
+            <div className="space-y-4 mt-6 overflow-y-auto">
+              <div className="space-y-2 p-4 bg-gray-50 rounded-lg overflow-x-auto whitespace-pre-wrap break-all">
+                <p className="text-sm text-gray-600">
+                  {t('your-access-key')}:
+                  <span className="block mt-1 font-mono text-red-500">
+                    {key}
+                  </span>
+                </p>
+                <p className="text-sm text-orange-500">
+                  {t('access-key-warning')}
+                </p>
+              </div>
+              <div className="space-y-2 p-4 bg-gray-50 rounded-lg overflow-x-auto whitespace-pre-wrap break-all">
+                <p className="text-sm text-gray-600">
+                  {t('public-access-URL')}:
+                  <a
+                    href={publicUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block mt-1 font-mono text-blue-500 hover:underline"
+                  >
+                    {publicUrl}
+                  </a>
+                </p>
+              </div>
+
+              <div className="space-y-2 p-4 bg-gray-50 rounded-lg">
+                <p className="text-sm text-gray-600">
+                  {t('embed-code')}:
+                  <code className="block mt-1 p-2 bg-gray-100 rounded font-mono text-sm overflow-x-auto whitespace-pre-wrap break-all">
+                    {embedScript}
+                  </code>
+                </p>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-
-      {/* Decode Key */}
-      <div className="space-y-4 pt-6 border-t">
-        <div className="space-y-2">
-          <Input
-            value={userKey}
-            onChange={(e) => setUserKey(e.target.value)}
-            placeholder="Enter access key"
-            className="max-w-md h-10"
-          />
-          <Button onClick={handleDecodeKey}>Decode Key</Button>
+          )}
         </div>
-
-        {decodedKey && (
-          <div className="p-4 bg-gray-50 rounded-lg">
-            <p className="text-sm text-gray-600">
-              Decoded key:{' '}
-              <span className="font-mono text-lg text-red-500">
-                {decodedKey}
-              </span>
-            </p>
-          </div>
-        )}
-      </div>
+      </Card>
     </div>
   );
 };
