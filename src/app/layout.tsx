0511c06297337f6363774b098bbafc07d02f2fda
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-21 14:40:25 +0100

    fix: cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 // Since we have a root `not-found.tsx` page, a layout file
 // is required, even if it's just passing children through.
-import { setSentryServiceTag } from './lib/services/sentry';
-
 export default function RootLayout({
   children,
 }: {
