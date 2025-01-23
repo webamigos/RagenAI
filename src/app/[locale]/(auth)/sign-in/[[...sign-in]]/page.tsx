78e6861835b9e56905d9d11926501b7f46e5f9ca
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-23 20:36:46 +0100

    fix: robots container

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({ params: { locale } }: PropsWihLocale) {
 export default function SignInPage() {
   const t = useTranslations('sign-in');
   return (
-    <div className="flex min-h-full flex-1">
+    <div className="flex min-h-screen flex-1">
       <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
@@ -45,13 +45,13 @@ export default function SignInPage() {
           </div>
         </div>
       </div>
-      <div className="relative hidden w-0 flex-1 lg:block lg:justify-end">
+      <div className="relative hidden lg:block lg:w-1/2">
         <Image
-          className="absolute inset-0 size-full object-cover h-full ml-8  dark:opacity-10 opacity-20"
+          className="absolute inset-0 w-full h-full object-cover dark:opacity-10 opacity-20"
           src="/assets/robot-3.png"
+          fill
+          sizes="(max-width: 768px) 0px, 50vw"
           alt=""
-          width={800}
-          height={600}
         />
       </div>
     </div>
