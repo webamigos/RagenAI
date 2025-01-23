5838a7a3008e42a042834617d072be6bb6d9a3ec
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-23 20:26:25 +0100

    fix: mobile view

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -19,43 +19,41 @@ export async function generateMetadata({ params: { locale } }: PropsWihLocale) {
 export default function SignInPage() {
   const t = useTranslations('sign-in');
   return (
-    <>
-      <div className="flex min-h-full flex-1">
-        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
-              <Logo className="h-16" disableLink />
-              <h2 className="mt-8 text-2xl/9 font-bold tracking-tight  dark:text-gray-300 text-gray-900">
-                {t('sign-in-to-account')}
-              </h2>
-              <p className="mt-2 text-sm/6 dark:text-gray-300 text-gray-500">
-                {t('not-a-member')}{' '}
-                <Link
-                  href="/sign-up"
-                  className="font-semibold dark:text-indigo-400 text-indigo-600 hover:text-indigo-500"
-                >
-                  {t('start-free-trial')}
-                </Link>
-              </p>
-            </div>
+    <div className="flex min-h-full flex-1">
+      <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <Logo className="h-16" disableLink />
+            <h2 className="mt-8 text-2xl/9 font-bold tracking-tight  dark:text-gray-300 text-gray-900">
+              {t('sign-in-to-account')}
+            </h2>
+            <p className="mt-2 text-sm/6 dark:text-gray-300 text-gray-500">
+              {t('not-a-member')}{' '}
+              <Link
+                href="/sign-up"
+                className="font-semibold dark:text-indigo-400 text-indigo-600 hover:text-indigo-500"
+              >
+                {t('start-free-trial')}
+              </Link>
+            </p>
+          </div>
 
-            <div className="mt-8">
-              <LoginForm />
+          <div className="mt-8">
+            <LoginForm />
 
-              <SocialAuthOptions isSignUp={false} />
-            </div>
+            <SocialAuthOptions isSignUp={false} />
           </div>
         </div>
-        <div className="relative hidden w-0 flex-1 lg:block lg:justify-end">
-          <Image
-            className="absolute inset-0 size-full object-cover h-full ml-8  dark:opacity-10 opacity-20"
-            src="/assets/robot-3.png"
-            alt=""
-            width={800}
-            height={600}
-          />
-        </div>
       </div>
-    </>
+      <div className="relative hidden w-0 flex-1 lg:block lg:justify-end">
+        <Image
+          className="absolute inset-0 size-full object-cover h-full ml-8  dark:opacity-10 opacity-20"
+          src="/assets/robot-3.png"
+          alt=""
+          width={800}
+          height={600}
+        />
+      </div>
+    </div>
   );
 }
