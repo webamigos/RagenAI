e5cd9087b3c0b9aeea1479c23cd0b380f26b07c2
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2025-02-11 07:56:40 +0100

    fix: fix

--- src/app/components/Sidebar/ThreadsHistory/ThreadsSection.tsx
+++ src/app/components/Sidebar/ThreadsHistory/ThreadsSection.tsx
@@ -43,6 +43,7 @@ export const ThreadsSection = ({
                 const isLastThreadInAllCategories =
                   categoryIndex === threadCategories.length - 1 &&
                   index === threads.length - 1;
+
                 return (
                   <div
                     className="first-of-type:mt-1.5 last-of-type:mb-2"
