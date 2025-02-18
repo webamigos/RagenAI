c811055f6eb742287748dec1892bad1037a1d583
Mateusz Janowski <m.janowski@tubics.com>
2025-02-18 11:05:57 +0100

    refactor: cleanup, remove OLLAMA_BASE_URL

--- src/validateEnvVars.js
+++ src/validateEnvVars.js
@@ -60,9 +60,6 @@ const envSchema = z.object({
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_SECRET_DOCUMENTS_BUCKET: z.string(),
 
-  // OLLAMA
-  OLLAMA_BASE_URL: z.string(),
-
   // Stripe
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
   STRIPE_SECRET_KEY: z.string(),
