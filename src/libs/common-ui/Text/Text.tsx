7dcc60220702deee465e82e62ea6999f41a714aa
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2025-02-26 17:02:07 +0100

    refactor: simplify Text and Dialog component styling

--- src/libs/common-ui/Text/Text.tsx
+++ src/libs/common-ui/Text/Text.tsx
@@ -3,19 +3,12 @@ import { classMerge } from '../utils/cn';
 
 type FontWeight = 'light' | 'normal' | 'medium' | 'semibold' | 'bold';
 type FontSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-type Color =
-  | 'zinc-950'
-  | 'blue-600'
-  | 'gray-400'
-  | 'gray-500'
-  | 'gray-700'
-  | 'red-500';
 
 type Props = {
   children: ReactNode | number;
   fontWeight?: FontWeight;
   fontSize?: FontSize;
-  color?: Color;
+  color?: string;
 };
 
 const fontWeightMap: Record<FontWeight, string> = {
@@ -34,27 +27,18 @@ const fontSizeMap: Record<FontSize, string> = {
   xl: 'text-xl',
 };
 
-const colorMap: Record<Color, string> = {
-  'zinc-950': 'text-zinc-950',
-  'blue-600': 'text-blue-600',
-  'gray-700': 'text-gray-700',
-  'gray-500': 'text-gray-500',
-  'gray-400': 'text-gray-400',
-  'red-500': 'text-red-500',
-};
-
 export const Text = ({
   children,
   className,
   fontWeight = 'normal',
   fontSize = 'md',
-  color = 'zinc-950',
+  color = 'text-zinc-950 dark:text-gray-200',
   ...rest
 }: ComponentProps<'p'> & Props) => {
   const classNames = classMerge(
     fontWeightMap[fontWeight],
     fontSizeMap[fontSize],
-    colorMap[color],
+    color,
     className
   );
 
