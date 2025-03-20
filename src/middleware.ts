c03e1f869fd66515da5492f4b4f1aebd75f0032e
KubaSzewczyk <kuba.szewczyk27@gmail.com>
2025-03-20 16:19:56 +0100

    fix:  add user authentication in messages route

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,6 @@ const PROTECTED_ROUTES = ['/:locale/threads/:threadId', '/admin'];
 const isProtectedRoute = createRouteMatcher(PROTECTED_ROUTES);
 const LOCALE_PREFIX_REGEX = /^\/(pl|en)/;
 const SIGN_IN_PATH = '/sign-in';
-const MESSAGES_ENDPOINT = `/api/messages/`;
 // export const config = {
 //   matcher: ['/', '/(pl|en)/:path*'],
 // };
@@ -64,7 +63,6 @@ export const config = {
     '/api/settings/prompt',
     '/api/send',
     '/api/upload/(.*)',
-    '/api/messages/(.*)',
     '/:locale/admin/manage-knowledge',
     '/:locale/sso-callback',
     '/:locale/sign-in',
@@ -98,12 +96,6 @@ export default clerkMiddleware(
     }
 
     if (url.startsWith('/api')) {
-      // Add protection for messages endpoint
-      if (url.includes(MESSAGES_ENDPOINT)) {
-        if (!session.userId) {
-          return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-        }
-      }
       return NextResponse.next();
     }
 
