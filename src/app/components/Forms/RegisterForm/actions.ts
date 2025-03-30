04a0f35ef87bd4009d30a5f9cd1baa6335398d4c
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-03-31 00:15:18 +0200

    fix: with blocking mails

--- src/app/components/Forms/RegisterForm/actions.ts
+++ src/app/components/Forms/RegisterForm/actions.ts
@@ -1,26 +1,25 @@
 'use server';
 
-import axios from 'axios';
 import { logger } from '@/app/lib/utils/logger';
 
 export const addSubscriberToKit = async (email: string) => {
   const KIT_API_KEY = process.env.KIT_API_KEY;
+
   try {
-    const response = await axios.post(
-      'https://api.kit.com/v4/subscribers',
-      {
-        email_address: email,
-      },
-      {
+    if (KIT_API_KEY) {
+      // there is a problem with axios in this server action and breaks flow
+      await fetch('https://api.kit.com/v4/subscribers', {
+        method: 'POST',
+        body: JSON.stringify({
+          email_address: email,
+        }),
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
-          'x-kit-api-key': `Bearer ${KIT_API_KEY}`,
+          'x-kit-api-key': KIT_API_KEY,
         },
-      }
-    );
-
-    return response;
+      });
+    }
   } catch (err) {
     logger.error({ err }, `Cannot add ${email} to kit.com`);
   }
