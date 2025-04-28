6d9c1c337bc07ea62b89774cd0e0b9ea19d04857
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-04-28 20:57:53 +0200

    feat: temporal client refactor

--- src/libs/temporal/client.ts
+++ src/libs/temporal/client.ts
@@ -2,23 +2,28 @@ import { Client, Connection } from '@temporalio/client';
 import { TEMPORAL_SERVER_ADDRESS, TEMPORAL_NAMESPACE } from './consts';
 
 const createClient = (): Client => {
-  const cert = process.env.TEMPORAL_CERT; // pem
-  const key = process.env.TEMPORAL_KEY; // key
+  // For Temporal Cloud
+  // const cert = process.env.TEMPORAL_CERT; // pem
+  // const key = process.env.TEMPORAL_KEY; // key
 
-  if (!cert || !key) {
-    throw new Error('Missing required Temporal certificates');
-  }
+  // if (!cert || !key) {
+  //   throw new Error('Missing required Temporal certificates');
+  // }
 
   const connection = Connection.lazy({
     address: TEMPORAL_SERVER_ADDRESS,
-    tls: {
-      clientCertPair: {
-        crt: Buffer.from(cert, 'base64'),
-        key: Buffer.from(key, 'base64'),
-      },
-    },
+    // For Temporal Cloud
+    // tls: {
+    //   clientCertPair: {
+    //     crt: Buffer.from(cert, 'base64'),
+    //     key: Buffer.from(key, 'base64'),
+    //   },
+    // },
   });
-  return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
+
+  // For Temporal Cloud
+  // return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
+  return new Client({ connection });
 };
 
 export const getTemporalClient = (): Client => {
