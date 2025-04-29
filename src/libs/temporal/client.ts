dfb1bf5e4338ac22e6053d77ebd68e3ea5948142
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-04-29 12:48:30 +0200

    fix: go back to temporal cloud

--- src/libs/temporal/client.ts
+++ src/libs/temporal/client.ts
@@ -1,29 +1,25 @@
 import { Client, Connection } from '@temporalio/client';
-import { TEMPORAL_SERVER_ADDRESS } from './consts';
+import { TEMPORAL_NAMESPACE, TEMPORAL_SERVER_ADDRESS } from './consts';
 
 const createClient = (): Client => {
-  // For Temporal Cloud
-  // const cert = process.env.TEMPORAL_CERT; // pem
-  // const key = process.env.TEMPORAL_KEY; // key
+  const cert = process.env.TEMPORAL_CERT; // pem
+  const key = process.env.TEMPORAL_KEY; // key
 
-  // if (!cert || !key) {
-  //   throw new Error('Missing required Temporal certificates');
-  // }
+  if (!cert || !key) {
+    throw new Error('Missing required Temporal certificates');
+  }
 
   const connection = Connection.lazy({
     address: TEMPORAL_SERVER_ADDRESS,
-    // For Temporal Cloud
-    // tls: {
-    //   clientCertPair: {
-    //     crt: Buffer.from(cert, 'base64'),
-    //     key: Buffer.from(key, 'base64'),
-    //   },
-    // },
+    tls: {
+      clientCertPair: {
+        crt: Buffer.from(cert, 'base64'),
+        key: Buffer.from(key, 'base64'),
+      },
+    },
   });
 
-  // For Temporal Cloud
-  // return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
-  return new Client({ connection });
+  return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
 };
 
 export const getTemporalClient = (): Client => {
