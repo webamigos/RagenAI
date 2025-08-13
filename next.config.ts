d9c1ca01674d6b8b59677d405367434b1fe15013
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-08-13 23:19:20 +0200

    fix: sentry config;

--- next.config.ts
+++ next.config.ts
@@ -123,7 +123,7 @@ export default !(isProductionTargetEnv || isStagingTargetEnv)
       // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
       // Upload a larger set of source maps for prettier stack traces (increases build time)
-      // widenClientFileUpload: true,
+      widenClientFileUpload: true,
 
       // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
       // This can increase your server load as well as your hosting bill.
@@ -131,15 +131,12 @@ export default !(isProductionTargetEnv || isStagingTargetEnv)
       // side errors will fail.
       // tunnelRoute: "/monitoring",
 
-      // Hides source maps from generated client bundles
-      // hideSourceMaps: true,
-
       // Automatically tree-shake Sentry logger statements to reduce bundle size
-      // disableLogger: true,
+      disableLogger: true,
 
       // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
       // See the following for more information:
       // https://docs.sentry.io/product/crons/
       // https://vercel.com/docs/cron-jobs
-      // automaticVercelMonitors: false,
+      automaticVercelMonitors: true,
     });
