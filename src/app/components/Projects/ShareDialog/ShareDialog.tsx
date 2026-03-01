c64777ef8a753662ffec6bf7b7936cca1007979a
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-03-01 19:31:34 +0100

    fix: tests

--- src/app/components/Projects/ShareDialog/ShareDialog.tsx
+++ src/app/components/Projects/ShareDialog/ShareDialog.tsx
@@ -28,7 +28,9 @@ type ShareDialogProps = {
   onChatbotEnabledChange?: (enabled: boolean) => Promise<boolean>;
 };
 
-const BASE_URL = `${window.location.origin}/pl/public/assistants`;
+function getBaseUrl() {
+  return `${window.location.origin}/pl/public/assistants`;
+}
 
 export const ShareDialog = ({
   open,
@@ -41,7 +43,7 @@ export const ShareDialog = ({
   onChatbotEnabledChange,
 }: ShareDialogProps) => {
   const [isSharedLinkPublicly, setIsSharedLinkPublicly] = useState(
-    isPublicProject || false
+    isPublicProject || false,
   );
   const [shareUrl, setShareUrl] = useState('');
   const wasPublicLinkKeyGenerated = useRef<boolean>(false);
@@ -51,7 +53,7 @@ export const ShareDialog = ({
   const [currentPublishedAt, setCurrentPublishedAt] = useState(publishedAt);
 
   const [isChatbotEnabled, setIsChatbotEnabled] = useState(
-    initialChatbotEnabled
+    initialChatbotEnabled,
   );
   const [isChatbotCustomized, setIsChatbotCustomized] = useState(false);
   const [accessKey, setAccessKey] = useState('');
@@ -113,7 +115,7 @@ export const ShareDialog = ({
       if (!shareUrl) {
         const accessToken = await generateTokenAndSetUrl(false);
         if (accessToken) {
-          setShareUrl(`${BASE_URL}/${accessToken}`);
+          setShareUrl(`${getBaseUrl()}/${accessToken}`);
         } else {
           setIsSharedLinkPublicly(false);
         }
@@ -224,7 +226,7 @@ export const ShareDialog = ({
             shareUrl={shareUrl}
             isGeneratingKey={isGeneratingKey}
             wasKeyGenerated={wasPublicLinkKeyGenerated.current}
-            linkToPublicProject={`${BASE_URL}/${currentLinkToPublicProject}`}
+            linkToPublicProject={`${getBaseUrl()}/${currentLinkToPublicProject}`}
             publishedAt={currentPublishedAt}
             projectId={projectId}
             onToggle={handleShareToggle}
