f310715367419545ccfbcbd4b8cc060d4ca2d277
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-03-14 17:09:23 +0100

    fix: build in container

--- src/libs/payments/stripe.ts
+++ src/libs/payments/stripe.ts
@@ -3,4 +3,7 @@ import 'server-only';
 import Stripe from 'stripe';
 
 // CONFIGURATION options: https://github.com/stripe/stripe-node#configuration
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY || 'sk_placeholder_for_build',
+  {},
+);
