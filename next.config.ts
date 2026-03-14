045746c2f077178902769eb23fef1b5b728e1ce3
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-03-14 11:35:34 +0100

    fix: build on ci

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ if (IS_API_MODE) {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'standalone' as const,
   reactStrictMode: true,
 
   async rewrites() {
